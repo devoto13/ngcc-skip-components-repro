c6f23d94173927b4adf044a8a82b094282dcdfa7
a <a>
2019-09-28 21:16:12 +0200

    break ngcc

--- projects/test/src/lib/test.component.ts
+++ projects/test/src/lib/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'lib-test',
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
